e4a223596174ed052912620e290e485bbfa445f4
MomchilDeus <momchil.kolev@protonmail.com>
2018-04-30 11:29:43 +0300

    dominantDirection handle empty string

--- Chapter5/dominantDirection.js
+++ Chapter5/dominantDirection.js
@@ -1,8 +1,11 @@
 // Though the nesting is awkward, I'm reusing characterScript
 function dominantDirection(text) {
-  let script = countBy(text, char => {
-    let script = characterScript(char.codePointAt(0))
-    return script ? script : "none"
-  }).sort((a, b) => b.count - a.count)[0]
+	if (!text) return 'Please provide a valid string'
+	else {
+		let script = countBy(text, char => {
+			let script = characterScript(char.codePointAt(0))
+			return script ? script : "none"
+		}).sort((a, b) => b.count - a.count)[0]
+	}
   return script.name.direction
 }
